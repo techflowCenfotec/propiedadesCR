ad6e150ef06c971b72703c65c634b92932caf0f7
Jorge Arguedas <jorge.argds@gmail.com>
2016-03-14 20:04:32 -0600

    Se levanta el usuario con el rol y los permisos

--- propiedadesCR/src/main/java/com/techflow/propiedadesCR/ejb/Trole.java
+++ propiedadesCR/src/main/java/com/techflow/propiedadesCR/ejb/Trole.java
@@ -2,6 +2,9 @@ package com.techflow.propiedadesCR.ejb;
 
 import java.io.Serializable;
 import javax.persistence.*;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
 import java.util.List;
 
 
@@ -56,6 +59,7 @@ public class Trole implements Serializable {
 			@JoinColumn(name="id_permission")
 			}
 		)
+	@JsonIgnore
 	public List<Tpermission> getTpermissions() {
 		return this.tpermissions;
 	}
--- propiedadesCR/src/main/java/com/techflow/propiedadesCR/services/LoginService.java
+++ propiedadesCR/src/main/java/com/techflow/propiedadesCR/services/LoginService.java
@@ -21,7 +21,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.techflow.propiedadesCR.contracts.LoginRequest;
+import com.techflow.propiedadesCR.ejb.Trole;
 import com.techflow.propiedadesCR.ejb.Tuser;
+import com.techflow.propiedadesCR.pojo.RolePOJO;
 import com.techflow.propiedadesCR.pojo.UserPOJO;
 import com.techflow.propiedadesCR.repositories.LoginRepository;
 
@@ -65,6 +67,11 @@ public class LoginService implements LoginServiceInterface {
 
 		if (null != nuser){
 			userPOJO = new UserPOJO();
+			Trole role =nuser.getTrole();	
+			RolePOJO rolePOJO = new RolePOJO();
+			BeanUtils.copyProperties(role, rolePOJO);
+			userPOJO = new UserPOJO();
+			userPOJO.setRole(rolePOJO);
 			BeanUtils.copyProperties(nuser, userPOJO);
 		}
 		return userPOJO;
