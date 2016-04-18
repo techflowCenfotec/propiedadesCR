54f167352385d9f059cf5015731875cc2cc5b874
Jorge Arguedas <jorge.argds@gmail.com>
2016-04-18 01:34:36 -0600

    Se agrego el landing page

--- propiedadesCR/src/main/webapp/resources/app/templates/homeView/home.js
+++ propiedadesCR/src/main/webapp/resources/app/templates/homeView/home.js
@@ -2,18 +2,11 @@
 	"use strict";
 
 	angular.module("app.home",[])
-	.controller('HomeController', ['$scope','$rootScope','$http','dbService','$location', function($scope,$rootScope,$http,dbService,$location) {
+	.controller('HomeController', ['$scope','$rootScope','$http','dbService', function($scope,$rootScope,$http,dbService) {
 		
 		$("#myCarousel").carousel();
-			console.log(localStorage.getItem('load'));
-			if(localStorage.getItem('load')!=='1'){
-				console.log('Entra');
-				localStorage.setItem('load','1');
-				$location.path('home');
-			}
 		
-		
-		
+	
 		dbService.checkDB();
 		
 	}]);
